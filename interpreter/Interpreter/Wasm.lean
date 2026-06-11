e61278fc695b2494235a12d91648a7061edd725e
Isabelle9999 <isabelle.lu930@icloud.com>
2026-06-11 11:06:37 -0400

    add table write instructions (table.set/grow/fill/copy/init, elem.drop)

--- interpreter/Interpreter/Wasm.lean
+++ interpreter/Interpreter/Wasm.lean
@@ -23,6 +23,7 @@ import Interpreter.Wasm.Examples.Switch
 import Interpreter.Wasm.Examples.SelectMin
 import Interpreter.Wasm.Examples.RefIsNull
 import Interpreter.Wasm.Examples.TableDispatch
+import Interpreter.Wasm.Examples.TableSet
 import Interpreter.Wasm.Examples.EarlyReturn
 import Interpreter.Wasm.Examples.EarlyBr
 import Interpreter.Wasm.Examples.EarlyBrInvalid
